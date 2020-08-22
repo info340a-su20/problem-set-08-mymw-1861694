7ac00acc7a40f9e3257453aef604ad25783d01b8
Michelle Wong Yu Miin <mymw@uw.edu>
2020-08-21 22:00:55 -0700

    Warnings?

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'; //import React Component
+import { render } from 'react-dom';
 
 const EXAMPLE_SENATORS = [  
   { id: 'C000127',  name: 'Maria Cantwell', state: 'WA',  party: 'Democrat', phone: '202-224-3441', twitter: 'SenatorCantwell' },
@@ -6,12 +7,39 @@ const EXAMPLE_SENATORS = [
 ];
 
 /* Your code goes here */
-export class App extends Component {
+export class App extends React.Component {
   render() {
-    let div = React.createElement('div', {className: 'container'});
-    let h1 = <h1>"US Senators 2019"</h1>;
-    div.appendChild(h1);
+    let div = (<div className='container'>
+      <h1>"US Senators 2019"</h1>
+      {SenatorTable.render()};
+      </div>);
     return div;
   }
-  
 } 
+
+export class SenatorTable extends React.Component {
+  render() {
+    let table = <table className='table table-bordered'></table>
+  }
+}
+
+//prop called cols that is an array of column names (as Strings).
+export class TableHeader extends React.Component {
+  render() {
+    <thead>
+      <tr>
+        <th>
+          //one for each String in the cols prop.
+        </th>
+      </tr>
+    </thead>
+  }
+}
+
+
+Use the .map() function to convert the cols prop into an array of <th> DOM elements (using JSX and inline expressions), and then include this array in the returned DOM elements.
+
+    Be sure to give each <th> element a key attribute (the column name string is a fine value) so that React can keep track of it.
+
+Include an instance of the TableHeader class in DOM rendered by the SenatorTable class (as a child of the <table>). The SenatorTable class should pass the TableHeader it creates a cols prop that is the array ['Name', 'State', 'Phone', 'Twitter'].
+
